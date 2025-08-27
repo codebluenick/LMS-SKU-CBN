c4502349f4ec249a888a3912b9b753a266a6ba74
codebluenick <codebluenick@gmail.com>
2025-08-26 22:38:18 -0700

    Add base layout to dashboard
    adds left and right columns for menu and dash view

--- Dashboard_UI/src/app/dashboard/layout.tsx
+++ Dashboard_UI/src/app/dashboard/layout.tsx
@@ -15,6 +15,11 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }>) {
   return (
-      <div>Dashboard{children}</div>
+      <div className="h-screen flex">
+        {/* LEFT (Horizontal menu layout)*/}
+        <div className="w-[14%] md:w[8%] lg:w[16%] xl:w[14%] bg-red-200">l</div>
+        {/* RIGHT (Dashboard view layout)*/}
+        <div className="w-[86%] md:w[8%] lg:w[84%] xl:w[86%%] bg-blue-200">r</div>
+      </div>
   );
 }
